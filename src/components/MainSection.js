07a03c1ead331a9f3e4d499dcc0fe041f048bda8
AnnaBilokon <Agnewka89@gmail.com>
2023-03-17 09:56:09 +0100

    add new Route

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -2,20 +2,23 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './MainSection.css';
+import video from '../video/galaxy-video-2.mp4';
 
 function MainSection() {
   return (
 	<div className='main-container'>
-	  <video src='/public/video/galaxy-video-1.mp4' autoPlay loop muted />
+	  <video src={video} autoPlay loop />
+	  
+	  <img src='../../public/images/galaxy-1.jpg' alt=''></img>
 	  <p>That's one small step for a man, one giant leap for mankind. </p>
 	<div className='main-btns'>
 		<Button className='btns' buttonStyle='btn--outline'
 		buttonSize='btn--large'>GET STARTED</Button>
 	<Button className='btns' buttonStyle='btn--primery'
-		buttonSize='btn--large'>WATCH TRAILER <i className='far=fa-play-circle'/> </Button>
+		buttonSize='btn--large'> <i className='far=fa-play-circle'/> WATCH TRAILER </Button>
 	</div>
 	  	</div>
   )
 }
 
-export default MainSection
+export default MainSection;
