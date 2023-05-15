5a58cb78aab7150c7acdef6836d8b29dc81e6207
AnnaBilokon <Agnewka89@gmail.com>
2023-05-15 11:35:54 +0200

    added new routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home.js';
+import SignUp from './components/pages/SignUp';
+import Articles from './components/pages/Articles.js';
+import News from './components/pages/News';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
 	<Navbar/>
 	<Routes>
 		<Route path='/' exact element={<Home/>} />
+		<Route path='/articles'  element={<Articles/>} />
+		<Route path='/news'  element={<News/>} />
+		<Route path='/sign-up' element={<SignUp/>} />
 	</Routes>
 	</Router>
 
